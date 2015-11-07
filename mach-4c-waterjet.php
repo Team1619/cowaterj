a08a3a5d4c7b14a91beea2ccb9eb08b260000674
NoahF10 <noahfeldman9@gmail.com>
2015-11-07 15:36:01 -0700

    Test

--- mach-4c-waterjet.php
+++ mach-4c-waterjet.php
@@ -4,7 +4,7 @@ include('header.php'); ?>
 <iframe width="300" height="225" src="http://www.youtube.com/embed/JjHYSqBnzCk?rel=0" frameborder="0" allowfullscreen></iframe>
 <br><img src="/images/Mach4.jpg" width="300px" style="padding: 10px 0 20px 0px">
 </div>
-<p>Featuring the most advanced waterjet technology in the industry, the Mach 4C Waterjet is designed specifically for unique configurations and customization, using the advanced Dynamic Waterjet® XD technology for faster, more accurate bevel and 3D cutting.
+<p> Hello Featuring the most advanced waterjet technology in the industry, the Mach 4C Waterjet is designed specifically for unique configurations and customization, using the advanced Dynamic Waterjet® XD technology for faster, more accurate bevel and 3D cutting.
 <p>Featuring exclusive HyperPressure Technology, allowing for faster cuts and increased precision, the Flow Mach 4 includes advanced beveling and 3D capabilities. The Revolutionary Dynamic Waterjet XD provides for up to 60 degrees of motion, making it capable of cutting virtually any shape on any material in either 2D or 3D. Able to cut two to four times as fast as other similar technologies, Dynamic Waterjet automatically compensates for stream lag and taper, natural occurrences of the waterjet process.
 <img src="/images/Mach4_2.jpg" align="right" style="padding: 20px 0 20px 20px">
 <p>HyperJet pumps are rated for maximum pressure of 6,500 bar (94,000 psi) and can cut up to 50% faster than other waterjet products, making it the ultimate system and pump combination.
